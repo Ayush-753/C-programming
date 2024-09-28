798135d1df1388e8e7837c3bb6dc2cfd5840aef2
Ayush Kamath <ayushubkamath@gmail.com>
2024-09-28 23:20:11 +0530

    sizeof()

---
+++ Chapter1/02_space.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    float b;
+    char ch;
+    printf("The space is %zu\n", sizeof(a));
+    printf("The space is %zu\n", sizeof(b));
+    printf("The space is %zu\n", sizeof(ch));
+}
