bc10e75413037e65fe4d288cc20233bed4cbc46d
Ayush Kamath <ayushubkamath@gmail.com>
2024-09-28 23:15:59 +0530

    variables

--- Chapter1/01_variables.c
+++ Chapter1/01_variables.c
@@ -2,6 +2,9 @@
 
 
 int main() {
-    printf("Hello World");
+    int a=10;
+    // int a; variable declaration
+    // a=10;    variable initialization
+    printf("The number is %d", a);
     return 0;
 }
