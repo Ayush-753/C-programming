a962355974fc3f724bee902d9fa49b58ed1312c4
Ayush Kamath <ayushubkamath@gmail.com>
2024-09-29 19:25:25 +0530

    chapt2-practice

---
+++ Chapter2-Practice/01-problem1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    // which data type will 3.0/8-2 return ?
+
+    // 3.0/8 is evaluated first due to precedence which results in float then -2 hence answer is float
+    return 0;
+}
