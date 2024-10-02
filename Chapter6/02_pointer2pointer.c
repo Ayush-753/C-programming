cf5543ad71fe72d6c6ac2dcb7b17d47ac9380534
Ayush Kamath <ayushubkamath@gmail.com>
2024-10-02 22:59:12 +0530

    pointer2pointer

--- Chapter6/02_pointer2pointer.c
+++ Chapter6/02_pointer2pointer.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main() {
-    
+    int i = 10;
+    int* j = &i;
+    int** k = &j;
+    printf("The value of i is %p\n", i);
+    printf("The value of i is %p\n", *(&i));
+    printf("The value of i is %p\n", *j);
+    printf("The value of i is %p\n", **(&j));
+    printf("The value of i is %p\n", ***(&k));
+
     return 0;
 }
